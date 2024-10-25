295abe55571208ebaa63eb1abb0723792096d736
Gina Marieges <gina.marieges@gmail.com>
2024-10-25 17:28:34 +0200

    Add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "error",
+  },
+};
