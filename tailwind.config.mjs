4cab51b8d0190c1851c557fdc9f78aa2be9c280b
Gina Marieges <gina.marieges@gmail.com>
2024-11-09 12:01:19 +0100

    Add reset styles

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+export default {
+  content: ["./src/**/*.{astro,html,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
